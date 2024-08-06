64295a6d66da48f173e5d87928c324bc6db4c9d0
ByteNinja <pijayblog@gmail.com>
2024-08-06 09:19:00 +0700

    Update local-auth.guard.ts

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,12 +1,5 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {
-  async canActivate(context: ExecutionContext) {
-    const result = (await super.canActivate(context)) as boolean;
-    const request = context.switchToHttp().getRequest();
-    await super.logIn(request);
-    return result;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
