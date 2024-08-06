ca0bcd2a485058c7ab2ac5a99053da7dcf846431
ByteNinja <pijayblog@gmail.com>
2024-08-06 09:26:39 +0700

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Group } from '../../groups/entities/group.entity';
 
-@Entity()
+@Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  user_id: number;
 
   @Column({ unique: true })
   username: string;
 
   @Column()
   password: string;
+
+  @Column({ default: 0 })
+  login_attempts: number;
+
+  @Column({ type: 'timestamp', nullable: true })
+  last_login_attempt: Date;
+
+  @Column({ nullable: true })
+  remember_me_token: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => Group, group => group.user)
+  groups: Group[];
 }
