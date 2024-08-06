ce0d77b4ed356c2253fdcb44ef066759985de45a
ByteNinja <pijayblog@gmail.com>
2024-08-06 09:33:46 +0700

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.id };
+    const payload = { username: user.username, sub: user.user_id };
     return {
       access_token: this.jwtService.sign(payload),
     };
