3cbd4e1bd4ced0d8e028a9989574164b2d025494
ByteNinja <pijayblog@gmail.com>
2024-08-06 09:13:36 +0700

    Create users.service.ts

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async create(user: User): Promise<void> {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    await this.usersRepository.save(user);
+  }
+}
